d52ed0cf4c26146ba080c72ecf25ec7f3fe71403
Kaan <133776954+KaanKocaturkmen@users.noreply.github.com>
2023-06-09 02:37:55 +0300

    Add files via upload

---
+++ Dogyears.js
@@ -0,0 +1,12 @@
+//Normal human age to dog age. 
+let myAge = 20;
+let earlyYears = 2;
+earlyYears = earlyYears*10.5
+// or earlyYears *= 10.5;
+let laterYears = myAge-2;
+laterYears = laterYears*4;
+// or laterYears *= 4;
+let myAgeInDogYears = earlyYears + laterYears; 
+const myName = 'Kaan'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+console.log(earlyYears);
